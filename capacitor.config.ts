90aef2d8c7ccaee6da3061800bcc189f45cad2c0
DeeBest22 <faithabayomi18@gmail.com>
2026-02-14 14:57:32 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.insurai.app',
+  appName: 'Insur Access',
+  webDir: 'dist'
+};
+
+export default config;
